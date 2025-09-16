713045e7eda53dc35b61ebc420069abda153b107
Luna <105922175+ImLunaHey@users.noreply.github.com>
2025-09-16 22:10:13 +0100

    fix: alias clearsky package for expo

--- apps/akari/metro.config.js
+++ apps/akari/metro.config.js
@@ -5,6 +5,7 @@ const config = getDefaultConfig(__dirname);
 
 // Add path mapping for the internal packages
 config.resolver.alias = {
+  '@/clearsky-api': path.resolve(__dirname, '../../packages/clearsky-api'),
   '@/bluesky-api': path.resolve(__dirname, '../../packages/bluesky-api'),
   '@/tenor-api': path.resolve(__dirname, '../../packages/tenor-api'),
 };
